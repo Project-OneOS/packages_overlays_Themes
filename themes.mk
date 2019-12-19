2c1650e28e19890339aff4b55dbfd80bf87ed577
inderpreetsinghb <inderpreetbhangu2@gmail.com>
2019-12-19 17:29:36 +0530

    Create Our ThemeStub

---
+++ themes.mk
@@ -0,0 +1,47 @@
+# Copyright (C) 2019 AospExtended ROM
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+# Overlay ThemePicker to use our ThemeStub
+DEVICE_PACKAGE_OVERLAYS += \
+    packages/overlays/Themes/overlays
+
+PRODUCT_PACKAGES += \
+   OneThemeStub
+
+# Accents
+PRODUCT_PACKAGES += \
+	AmberAccent \
+	BlueAccent \
+	BlueGreyAccent \
+	BrownAccent \
+	CyanAccent \
+	CandyRedAccent \
+	DeepOrangeAccent \
+	DeepPurpleAccent \
+	ExtendedGreenAccent \
+	ElegantGreenAccent \
+	GreyAccent \
+	IndigoAccent \
+	JadeGreenAccent \
+	LightBlueAccent \
+	LightGreenAccent \
+	LimeAccent \
+	OrangeAccent \
+	PaleBlueAccent \
+	PaleRedAccent \
+	PinkAccent \
+	RedAccent \
+	TealAccent \
+	YellowAccent
